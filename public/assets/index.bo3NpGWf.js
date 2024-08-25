5f28fdaba6329895670ab53adc3023c79fb94409
Your Name <you@example.com>
2024-08-25 19:25:59 +0800

    77

---
+++ public/assets/index.bo3NpGWf.js
@@ -0,0 +1,1 @@
+import{a as o}from"./request.iDM26VIX.js";const t=()=>o({url:"index",method:"post"}),s=()=>o({url:"get/config",method:"post"});export{s as c,t as i};
---
+++ public/assets/question.CR2pb9Dg.js
@@ -0,0 +1,1 @@
+import{a as s}from"./request.iDM26VIX.js";const a=a=>s({url:"message/submit",method:"post",data:a,showToast:!0}),t=a=>s({url:"message",method:"post",data:a,showToast:!0}),o=a=>s({url:"message/list",method:"post",data:a,showToast:!0}),e=a=>s({url:"proclamation/list",method:"post",data:a,showToast:!0});export{o as a,t as m,e as p,a as s};
