9843c9b84afefbcc8fe8b8f2bf8c4787cb407d46
Your Name <you@example.com>
2025-02-20 10:04:02 +0800

    66

--- resources/views/admin/membertree.blade.php
+++ resources/views/admin/membertree.blade.php
@@ -20,9 +20,9 @@
                     <div class="col-md-2"></div>
 
                     <div class="col-md-8">
-                        <div class="btn-group pull-left">
-                            <button type="reset" class="btn btn-warning pull-right">重置</button>
-                        </div>
+{{--                        <div class="btn-group pull-left">--}}
+{{--                            <button type="reset" class="btn btn-warning pull-right">重置</button>--}}
+{{--                        </div>--}}
 
                         <div class="btn-group pull-right">
                             <button type="submit" class="btn btn-info pull-right">提交</button>
