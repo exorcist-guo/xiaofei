08ec8fd27fda05f5a9a619915fd6431efd520988
Your Name <you@example.com>
2025-02-27 17:13:35 +0800

    翻译

---
+++ resources/lang/en/namemap.php
@@ -0,0 +1,23 @@
+<?php 
+
+return [
+    '后台增加' => 'Background increment',
+    '转入' => 'Transfer to',
+    '激活增加' => 'Increased activation',
+    '后台转入' => 'Background transfer',
+    '推送' => 'Push notification',
+    '后台减少' => 'Background reduction',
+    '转出' => 'roll-out',
+    '激活减少' => 'Decreased activation',
+    '后台转出' => 'Background roll-out',
+    '商城推送' => 'Mall push',
+    '商城增加' => 'Mall increase',
+    '提现失败' => 'Withdrawal failure',
+    '极差奖励' => 'Range reward',
+    '推荐奖励' => 'Referral reward',
+    '服务奖励' => 'Service reward',
+    '服务补贴' => 'Service subsidy',
+    '促销奖励' => 'Promotional incentives',
+    '福利奖励' => 'Welfare reward',
+    '提现' => 'withdraw',
+];
